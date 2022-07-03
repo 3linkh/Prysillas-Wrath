5fac8b96a62e1f6a01fc00a8b70328b391d07751
Brendan Webster <bswebste@gmail.com>
2022-07-02 20:52:12 -0600

    Death and Heal Player
    Added death and heal script and added sfx/particle effects
    File cleanup - delete unused sfx

---
+++ Assets/Scripts/HealthPlayer2.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer2 : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 3;
+    [SerializeField] int damageAmount = 1;
+    [SerializeField] ParticleSystem healParticleSystem;
+    [SerializeField] AudioSource healAudioSource;
+
+    bool playerIsDead = false;
+
+    void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            TakeDamage(damageAmount);
+        }
+         
+    }    
+    
+    
+    void TakeDamage(int damageAmount)
+    {
+        hitPoints -= damageAmount;
+
+        if(hitPoints <= 0)
+        {
+            Debug.Log("player dies");
+            PlayerDies();
+        }
+    }
+
+    void PlayerDies()
+    {
+        gameObject.GetComponent<MovePlayer2>().enabled = false; 
+        playerIsDead = true;
+        //gameObject.GetComponent<SphereCollider>().isTrigger = true;
+        
+        // death animation
+        // death sfx
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (playerIsDead && other.gameObject.tag == "Player")
+        {
+            StartCoroutine("Healing");
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        StopCoroutine("Healing");    
+    }
+    IEnumerator Healing()
+    {
+        StartHealingAnimation();
+        yield return new WaitForSeconds(3f);
+        StopHealingAnimation();
+        HealPlayer();
+        
+    }
+
+    void StartHealingAnimation()
+    {
+        healParticleSystem.Play();
+        healAudioSource.Play();
+
+    }
+
+    void StopHealingAnimation()
+    {
+        healParticleSystem.Stop();
+        healAudioSource.Stop();
+    }
+
+    void HealPlayer()
+    {
+        hitPoints = 3;
+        gameObject.GetComponent<MovePlayer2>().enabled = true; 
+        //gameObject.GetComponent<SphereCollider>().isTrigger = false;
+        playerIsDead = false;
+    }
+
+
+}
